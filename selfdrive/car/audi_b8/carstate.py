1f3b98d212f6711ab64f86a3d58780159a89b6c6
crossfun <62176160+crossfun@users.noreply.github.com>
2023-03-19 16:17:01 +0100

    Update carstate.py

--- selfdrive/car/audi_b8/carstate.py
+++ selfdrive/car/audi_b8/carstate.py
@@ -57,15 +57,13 @@ class CarState(CarStateBase):
     if trans_type == TransmissionType.automatic:
       ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(pt_cp.vl["Getriebe_11"]["GE_Fahrstufe"], None))
     elif trans_type == TransmissionType.direct:
-	if bool(pt_cp.vl["Gateway_72"]["BCM1_Rueckfahrlicht_Schalter"]):
-      ret.gearShifter = GearShifter.reverse
     else:																					   
       ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(pt_cp.vl["TSK_01"]["GearPosition"], None))
     elif trans_type == TransmissionType.manual:
       ret.clutchPressed = not pt_cp.vl["Motor_14"]["MO_Kuppl_schalter"]
-      if bool(pt_cp.vl["Gateway_72"]["BCM1_Rueckfahrlicht_Schalter"]):
-        ret.gearShifter = GearShifter.reverse
-      else:
+	 if bool(pt_cp.vl["Gateway_72"]["BCM1_Rueckfahrlicht_Schalter"]):
+       ret.gearShifter = GearShifter.reverse
+       else:
         ret.gearShifter = GearShifter.drive
 
     # Update door and trunk/hatch lid open status.
